cef4f9f5bd47e41ab5b1c883354967f7f11cc8ee
英豪 李 <5b1g0009@stust.edu.tw>
2024-12-11 11:16:52 +0800

    更新 README.md 並修改 MainWindow 佈局和邏輯
    更新了 `README.md` 文件中的標題和內容，將原本的文字替換為亂碼。在 `MainWindow.xaml` 中，修改了 `Title` 屬性，調整了視窗的高度和寬度，新增了 `Grid.RowDefinitions` 和 `Grid.ColumnDefinitions`，並在 `Grid` 中新增了一個 `StackPanel`。在 `MainWindow.xaml.cs` 中，新增了 `aqiURL` 字串變數，並在建構函式中設置 `urlTextBox` 的文字，新增了一個 `btnGetAQI_Click` 方法但尚未實作。

--- WpfApp_20241211_air/MainWindow.xaml.cs
+++ WpfApp_20241211_air/MainWindow.xaml.cs
@@ -16,9 +16,16 @@ namespace WpfApp_20241211_air
     /// </summary>
     public partial class MainWindow : Window
     {
+        string aqiURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
         public MainWindow()
         {
             InitializeComponent();
+            urlTextBox.Text = aqiURL;
+        }
+
+        private void btnGetAQI_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
